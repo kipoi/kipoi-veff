a386870cba76078f03a61caad01c0f1519b0fdde
Roman Kreuzhuber <rkreuzh@gmail.com>
2018-07-17 14:12:43 +0100

    fix errors

--- kipoi_veff/cli.py
+++ kipoi_veff/cli.py
@@ -220,7 +220,7 @@ def cli_score_variants(command, raw_args):
                                                          std_var_id=args.std_var_id,
                                                          restriction_bed=args.restriction_bed,
                                                          return_predictions=keep_predictions,
-                                                         model_outputs = model_outputs)
+                                                         output_filter = model_outputs)
 
     # tabular files
     if keep_predictions:
