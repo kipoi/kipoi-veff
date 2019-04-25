ca5a8bf918c84a299de7b5bd6fa94a454357ebbc
DerThorsten <derthorstenbeier@gmail.com>
2019-04-25 15:41:21 +0200

    using most recent kipoi version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 
 requirements = [
-    "kipoi>=0.6.1",
+    "kipoi>=0.6.12",
     #$"git+ssh://git@github.com/DerThorsten/kipoi.git@niceparse",#egg=kipoi,
     "kipoi-utils>=0.1.12",
     # vep
